e9efca43c3110d4ce03a698e5841257632eee72e
Jacob Latonis <latonis@wisc.edu>
2022-07-01 19:36:21 -0500

    maybe ready?

--- score.go
+++ score.go
@@ -3,11 +3,20 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"log"
+	"io"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 )
+
+type Runner struct {
+	Fatigue      float64
+	Form         float64
+	Fitness      float64
+	FitnessDates map[string]int
+	FatigueDates map[string]int
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -21,36 +30,85 @@ func main() {
 	}
 	fmt.Println(absPath)
 
-	readCSV(absPath)
+	var a Runner
+	getDates(&a)
 
+	readCSV(absPath, &a)
+
+	fmt.Printf("Fitness\n>\t%f\nFatigue\n>\t%f\nForm\n>\t%f\n", a.Fitness, a.Fatigue, a.Form)
 }
 
-func readCSV(fname string) {
+func getDates(runner *Runner) {
+
+	dt := time.Now()
+
+	fitDates := make(map[string]int)
+
+	// fmt.Println(dt.Format("2006-01-02"))
+
+	for i := 0; i < 42; i++ {
+		newTime := dt.AddDate(0, 0, i*-1)
+		fitDates[newTime.Format("2006-01-02")] = 0
+	}
+
+	runner.FitnessDates = fitDates
+
+	fatigueDates := make(map[string]int)
+
+	for i := 0; i < 7; i++ {
+		newTime := dt.AddDate(0, 0, i*-1)
+		fatigueDates[newTime.Format("2006-01-02")] = 0
+	}
+
+	runner.FatigueDates = fatigueDates
+}
+
+func readCSV(fname string, runner *Runner) {
 	fp, err := os.Open(fname)
 	csvReader := csv.NewReader(fp)
+
+	if _, err := csvReader.Read(); err != nil {
+		panic(err)
+	}
 
 	if err != nil {
 		panic(err)
 	}
 
-	data, err := csvReader.ReadAll()
+	for {
+		rec, err := csvReader.Read()
 
-	if err != nil {
-		log.Fatal(err)
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		// TSS idx 22
+		// date idx 6
+
+		date := rec[5]
+		s, err := strconv.ParseFloat(rec[22], 32)
+
+		if err != nil {
+			panic(err)
+		}
+
+		if _, ok := runner.FitnessDates[date]; ok {
+			runner.Fitness += s
+		}
+
+		if _, ok := runner.FatigueDates[date]; ok {
+			runner.Fatigue += s
+		}
 	}
 
-	dt := time.Now()
+	runner.Fatigue = runner.Fatigue / 7
+	runner.Fitness = runner.Fitness / 42
 
-	dates := make(map[string]int)
-
-	fmt.Println(dt.Format("2006-01-02"))
-
-	for i := 0; i < 42; i++ {
-		newTime := dt.AddDate(0, 0, i*-1)
-		dates[newTime.Format("2006-01-02")] = 0
-	}
-
-	fmt.Println(dates)
+	runner.Form = runner.Fitness - runner.Fatigue
 
 	defer fp.Close()
 
